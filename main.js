8b42f06f0ebe68a870fce36fa68533ce2f577f95
Javier Lavandier Tejada <javierlavandier@gmail.com>
2023-07-06 10:53:20 -0400

    Aside adjust

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const carritoMenu = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContaiener = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
@@ -11,10 +11,10 @@ menuHamIcon.addEventListener('click', toogleMobileMenu)
 carritoMenu.addEventListener('click', toggleCarrito)
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive')
+    const isAsideClosed = shoppingCartContaiener.classList.contains('inactive')
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContaiener.classList.add('inactive')
     }
 
     desktopMenu.classList.toggle('inactive')
@@ -22,10 +22,10 @@ function toggleDesktopMenu() {
 
 function toogleMobileMenu() {
 
-    const isAsideClosed = aside.classList.contains('inactive')
+    const isAsideClosed = shoppingCartContaiener.classList.contains('inactive')
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContaiener.classList.add('inactive')
     }
 
     mobileMenu.classList.toggle('inactive')
@@ -42,7 +42,7 @@ function toggleCarrito() {
         desktopMenu.classList.add('inactive')
     }
 
-    aside.classList.toggle('inactive')
+    shoppingCartContaiener.classList.toggle('inactive')
 }
 
 const productList = []
